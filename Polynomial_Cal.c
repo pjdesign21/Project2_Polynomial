d9e0e9e616d4ae1175038218470b987b40b7b743
sam-loker <48886496+sam-loker@users.noreply.github.com>
2019-03-25 18:17:19 +0800

    version 1.1

--- Polynomial_Cal.c
+++ Polynomial_Cal.c
@@ -7,8 +7,17 @@
  * 
  * \date 2019/03/25 Fang :created
 ****************************************************************************/
-Poterm Add(Poterm First_term,term Add_term)
+#include "Polynomial_Cal" 
+Poterm ADD(Poterm First_term,Poterm Add_term)
 {
 }
 
+Poterm MINUS(Poterm First_term,Poterm Minus_term)
+{
+	
+} 
+Poterm MULTIPLE(Poterm First_term,Poterm Multiple_term)
+{
+	
+} 
 
---
+++ Polynomial_Interact.h
@@ -0,0 +1,12 @@
+/****************************************************************************
+ * \brief       
+ * \copyright   2018-2019, Zhejiang University, Program Design Project, Group 6. 
+ * \verson      1.0
+ * \author      
+ * \file        Polynomial_Interact.h
+ * 
+ * \date 2019/03/25 Fang :created
+****************************************************************************/
+#include "Polynomial_Base"
+void init();
+void output(Poterm t);
