cffeb9d9732bc88995aaf6e0eda26f17cd51bf3f
Dream4Leo <3180102688@zju.edu.cn>
2019-03-26 15:33:43 +0800

    calculator

--- Polynomial_Cal.c
+++ Polynomial_Cal.c
@@ -7,17 +7,80 @@
  * 
  * \date 2019/03/25 Fang :created
 ****************************************************************************/
+
 #include "Polynomial_Cal.h" 
-Poterm ADD(Poterm First_term,Poterm Add_term)
+
+static Poterm coreverse( Poterm TermTobeReverse )
 {
+    Poterm p = TermTobeReverse->next;
+    while ( p != NULL ) p->co *= -1, p = p->next;
+    return TermTobeReverse;
 }
 
-Poterm MINUS(Poterm First_term,Poterm Minus_term)
+static Poterm product( Poterm TermTobeMult, Poterm term )
 {
-	
-} 
-Poterm MULTIPLE(Poterm First_term,Poterm Multiple_term)
-{
-	
-} 
+    Poterm Result_term = malloc( sizeof( struct Term ) );
+    Poterm p = Result_term;
+    TermTobeMult = TermTobeMult->next;
+    while ( TermTobeMult != NULL )
+    {
+        Creat( p, TermTobeMult->x + term->x, TermTobeMult->y + term->y, 
+            TermTobeMult->co * term->co );
+        TermTobeMult = TermTobeMult->next;
+    }
+    return Result_term;
+}
 
+extern Poterm ADD( Poterm First_term, Poterm Add_term )
+{
+    Poterm Result_term = malloc( sizeof( struct Term ) );
+    Poterm p = Result_term, a = First_term, b = Add_term;
+
+    Result_term->next = NULL;
+    First_term = a->next;
+    Add_term = b->next;
+    free( a );
+    free( b );
+
+    while ( First_term != NULL && Add_term != NULL )
+    {
+        a = First_term, b = Add_term;
+        int32_t cmp = Powercmp( a, b );
+        if ( cmp == 0 )
+        {
+            if ( a->co + b->co != 0 )
+                Creat( p, a->x, a->y, a->co + a->co );
+            First_term = a->next, Add_term = b->next;
+            free( a ), free( b );
+        } else
+        {
+            if ( cmp == 0 ) a = b;   
+            p->next = a;
+            First_term = a->next;
+            free( a );
+        }
+        p = p->next;
+    }
+    if ( First_term != NULL ) Append( p, First_term );
+    else Append( p, Add_term );
+
+    return Result_term;
+}
+
+extern Poterm MINUS(Poterm First_term,Poterm Minus_term)
+{
+    return ADD( First_term, coreverse( Minus_term ) );
+}
+
+extern Poterm MULTIPLE(Poterm First_term,Poterm Multiple_term)
+{
+    Poterm Result_term = malloc( sizeof( struct Term ) );
+    Multiple_term = Multiple_term->next;
+
+	while ( Multiple_term != NULL )
+    {
+        Result_term = ADD( Result_term, product( First_term, Multiple_term ) );
+        Multiple_term = Multiple_term->next;
+    }
+    return Result_term;
+}
--- Polynomial_Cal.h
+++ Polynomial_Cal.h
@@ -7,32 +7,60 @@
  * 
  * \date 2019/03/25 Fang :created
 ****************************************************************************/
+
 #include "Polynomial_base.h"
+
+
+/****************************************************************************
+ * Function:    coreverse
+ * Brief:       coefficient -> -coefficient
+ * Param [in]:  Poterm TermTobeReverse
+ * Param [out]: Poterm TermTobeReverse
+ * Author:      Zhengze Liu
+ * Date:        2019/03/26
+****************************************************************************/
+static Poterm coreverse( Poterm TermTobeReverse );
+
+
+/****************************************************************************
+ * Function:    product
+ * Brief:       multiply with single term
+ * Param [in]:  Poterm TermTobeMult, Poterm term
+ * Param [out]: Poterm Result_term
+ * Author:      Zhengze Liu
+ * Date:        2019/03/26
+****************************************************************************/
+static Poterm product( Poterm TermTobeMult, Poterm term );
+
+
 /****************************************************************************
  * Function:    ADD
  * Brief:       add a term
  * Param [in]:  Poterm First_term,term Add_term
  * Param [out]: Poterm First_term
- * Author:      
+ * Author:      Zhengze Liu
  * Date:        2019/03/25
 ****************************************************************************/
-Poterm ADD(Poterm First_term,Poterm Add_term);
+extern Poterm ADD(Poterm First_term,Poterm Add_term);
+
+
 /****************************************************************************
  * Function:    MINUS
  * Brief:       minus a term
  * Param [in]:  Poterm First_term,term Minus_term
  * Param [out]: Poterm First_term
- * Author:      
+ * Author:      Zhengze Liu
  * Date:        2019/03/25
 ****************************************************************************/
-Poterm MINUS(Poterm First_term,Poterm Minus_term);
+extern Poterm MINUS(Poterm First_term,Poterm Minus_term);
+
+
 /****************************************************************************
  * Function:    MULTIPLE
  * Brief:       multiple a term
  * Param [in]:  Poterm First_term,term Multiple_term
  * Param [out]: Poterm First_term
- * Author:      
+ * Author:      Zhengze Liu
  * Date:        2019/03/25
 ****************************************************************************/
-Poterm MULTIPLE(Poterm First_term,Poterm Multiple_term);
-
+extern Poterm MULTIPLE(Poterm First_term,Poterm Multiple_term);
