6c097778f8fa34c0e3211e01c84570612249b3ca
Dream4Leo <3180102688@zju.edu.cn>
2019-03-26 10:57:52 +0800

    push?

---
+++ Polynomial_Interact.c
@@ -0,0 +1,18 @@
+/****************************************************************************
+ * \brief       
+ * \copyright   2018-2019, Zhejiang University, Program Design Project, Group 6. 
+ * \verson      1.0
+ * \author      
+ * \file        Polynomial_Interact.c
+ * 
+ * \date 2019/03/25 Fang :created
+****************************************************************************/
+#include "Polynomial_Interact.h"
+void init()
+{
+	
+}
+void output(Poterm t)
+{
+	
+}
---
+++ Polynomial_Interact.h
@@ -0,0 +1,29 @@
+/****************************************************************************
+ * \brief       
+ * \copyright   2018-2019, Zhejiang University, Program Design Project, Group 6. 
+ * \verson      1.0
+ * \author      
+ * \file        Polynomial_Interact.h
+ * 
+ * \date 2019/03/25 Fang :created
+****************************************************************************/
+//#include "Polynomial_Base.h"
+#include "Polynomial_Identify.c"
+/****************************************************************************
+ * Function:    init
+ * Brief:       form a interactive interface
+ * Param [in]:  NA
+ * Param [out]: NA
+ * Author:      
+ * Date:        2019/03/25
+****************************************************************************/
+void init();
+/****************************************************************************
+ * Function:    output
+ * Brief:       Output the answer 
+ * Param [in]:  Poterm t
+ * Param [out]: NA
+ * Author:      
+ * Date:        2019/03/25
+****************************************************************************/
+void output(Poterm t);
